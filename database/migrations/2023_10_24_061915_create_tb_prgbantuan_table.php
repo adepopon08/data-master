07092e72123c240d73554bd161616c14a5b3f0fc
Yanto Supriyanto <superiyanto.hungkul@gmail.com>
2023-10-25 14:07:38 +0700

    perbaikan tabel bantuan

--- database/migrations/2023_10_24_061915_create_tb_prgbantuan_table.php
+++ database/migrations/2023_10_24_061915_create_tb_prgbantuan_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('tb_prgbantuan', function (Blueprint $table) {
             $table->id();
+            $table->string('prgbantuan',10);
             $table->timestamps();
         });
     }
